dfc8cb5d223f6266b51efdca9fde26b8cecdfdbe
kathleen547 <blueberry8947@gmail.com>
2026-03-10 21:49:45 +0100

    implement project task table and filter UI

---
+++ app/static/js/main.js
@@ -0,0 +1,11 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+
+function func1() {
+  document.getElementById("filter-dropdown").classList.toggle("show");
+}
+
+function func2() {
+  document.getElementById("status-dropdown").classList.toggle("show");
+}
