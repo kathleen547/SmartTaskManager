d069e43b259ba24eeb08673455fd48824fa9cd44
kathleen547 <blueberry8947@gmail.com>
2026-03-21 21:19:28 +0100

    Add task search functionality

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -13,3 +13,57 @@ function func2() {
 function confirmDelete(){
     return confirm("Are you sure to delete this task?");
 }
+
+
+const table = document.getElementById("project-table");
+const tableBody = table.getElementsByTagName("tbody")[0];
+const rows = tableBody.querySelectorAll("tr");
+const flag = {Task: false, Status: false, Priority: false, DueDate: false, AssignedTo: false, Actions: false};
+let data = [];
+
+rows.forEach((row) => {
+const title = row.querySelector(".task-title").textContent;
+const status = row.querySelector(".task-status").textContent;
+const priority = row.querySelector(".task-priority").textContent;
+const dueDate = row.querySelector(".task-date").textContent;
+const assigned = row.querySelector(".task-owner").textContent;
+const actions = row.querySelector(".task-actions").textContent;
+
+let task = {
+    Task: title.trim().toLowerCase(),
+    Status: status.trim().toLowerCase(),
+    Priority: priority.trim().toLowerCase(),
+    DueDate: dueDate.trim(),
+    AssignedTo: assigned.trim(),
+    Actions: actions.trim(),
+    rowElement: row
+    };
+data.push(task);
+});
+
+function searchItems() {
+    let input = document
+        .getElementById("searchInput")
+        .value
+        .trim()
+        .toLowerCase();
+
+    let hasVisibleItems = false;
+
+    data.forEach((e) => {
+        if (e.Task.includes(input)) {
+            e.rowElement.style.display = "";
+            hasVisibleItems = true;
+        } else {
+            e.rowElement.style.display = "none";
+        }
+    });
+
+    const message = document.getElementById("noResultsMessage");
+
+    if (hasVisibleItems || input === "") {
+        message.style.display = "none";
+    } else {
+        message.style.display = "block";
+    }
+}
